9bd3a422f0655f3f730b66c365069758351f8c6e
Jyoti Suvarna <jyoti.suvarna@canopy.cloud>
2019-05-08 11:20:35 +0530

    Remove console logs CAN-908

--- app/components/people-list.js
+++ app/components/people-list.js
@@ -1,6 +1,11 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  didRender() {
+    this._super(...arguments);
+    // console.log('I rendered!');
+  },
+
   actions: {
     showPerson(person) {
       alert(person);
